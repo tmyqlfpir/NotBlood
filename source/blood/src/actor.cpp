tmyqlfpir/NotBlood

commit 5f064bed46fd8b5ea4071f168db3ca33b018f188

Clear gPost actions on actInit
Fix by @zZeck, thank you!

source/blood/src/actor.cpp
@@ -2405,8 +2405,8 @@ int nDudeToGibClient2 = seqRegisterClient(DudeToGibCallback2);
 int gPostCount = 0;
 
 struct POSTPONE {
-    short at0;
-    short at2;
+    short nSprite;
+    short nStatus;
 };
 
 POSTPONE gPost[kMaxSprites];
@@ -2497,7 +2497,8 @@ int DudeDifficulty[5] = {
 };
 
 void actInit(bool bSaveLoad) {
-    
+    memset(gPost, 0, sizeof(gPost));
+
     #ifdef NOONE_EXTENSIONS
     if (!gModernMap) {
         initprintf("> This map *does not* provide modern features.\n");
@@ -7731,7 +7732,7 @@ void actPostSprite(int nSprite, int nStatus)
     if (sprite[nSprite].flags&32)
     {
         for (n = 0; n < gPostCount; n++)
-            if (gPost[n].at0 == nSprite)
+            if (gPost[n].nSprite == nSprite)
                 break;
         dassert(n < gPostCount);
     }
@@ -7741,8 +7742,8 @@ void actPostSprite(int nSprite, int nStatus)
         sprite[nSprite].flags |= 32;
         gPostCount++;
     }
-    gPost[n].at0 = nSprite;
-    gPost[n].at2 = nStatus;
+    gPost[n].nSprite = nSprite;
+    gPost[n].nStatus = nStatus;
 }
 
 void actPostProcess(void)
@@ -7750,10 +7751,10 @@ void actPostProcess(void)
     for (int i = 0; i < gPostCount; i++)
     {
         POSTPONE *pPost = &gPost[i];
-        int nSprite = pPost->at0;
+        int nSprite = pPost->nSprite;
         spritetype *pSprite = &sprite[nSprite];
         pSprite->flags &= ~32;
-        int nStatus = pPost->at2;
+        int nStatus = pPost->nStatus;
         if (nStatus == kStatFree)
         {
             evKill(nSprite, 3);

source/blood/src/view.cpp
@@ -2552,9 +2552,9 @@ tspritetype *viewAddEffect(int nTSprite, VIEW_EFFECT nViewEffect)
             pNSprite->x += mulscale30((int)nDist, Cos(gCameraAng));
             pNSprite->y += mulscale30((int)nDist, Sin(gCameraAng));
             pNSprite->ang = gCameraAng;
+            break;
         }
-        else
-            pNSprite->z -= (pNSprite->yrepeat<<2)*(height-center);
+        pNSprite->z -= (pNSprite->yrepeat<<2)*(height-center);
         break;
     }
     case kViewEffectFlareHalo:

commit a48a5b2297401530d4803a903d79370994bbdab3

Only reset postpone status count on new level
Fixes crash on load file restore introduced by commit https://github.com/carnivoroussociety/NotBlood/commit/5f064bed46fd8b5ea4071f168db3ca33b018f188

source/blood/src/actor.cpp
@@ -2497,7 +2497,9 @@ int DudeDifficulty[5] = {
 };
 
 void actInit(bool bSaveLoad) {
-    memset(gPost, 0, sizeof(gPost));
+    if (!bSaveLoad) { // reset postpone count on new level
+        gPostCount = 0;
+    }
 
     #ifdef NOONE_EXTENSIONS
     if (!gModernMap) {