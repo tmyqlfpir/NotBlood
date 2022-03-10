nukeykt/NBlood

commit 08c35968b11f04f49d42477f29c3cbbd06ac4cd7

Documented DUDEEXTRA struct
Fixed enemies not resetting active state on new level (fixes death wish E2M9 bug with Cerberus dying at start)
Fix enemies always using tesla hit reaction after being hit with tesla projectile once
Added support for kDudeCerberusOneHead init idle state

source/blood/src/aiunicult.cpp
@@ -2188,8 +2188,8 @@ void genDudePostDeath(spritetype* pSprite, DAMAGE_TYPE damageType, int damage) {
 void aiGenDudeInitSprite(spritetype* pSprite, XSPRITE* pXSprite) {
     switch (pSprite->type) {
         case kDudeModernCustom: {
-            DUDEEXTRA_at6_u1* pDudeExtraE = &gDudeExtra[pSprite->extra].at6.u1;
-            pDudeExtraE->at8 = pDudeExtraE->at0 = 0;
+            DUDEEXTRA_STATS* pDudeExtraE = &gDudeExtra[pSprite->extra].stats;
+            pDudeExtraE->active = 0;
             aiGenDudeNewState(pSprite, &genDudeIdleL);
             break;
         }

source/blood/src/actor.cpp
@@ -2956,7 +2956,7 @@ void actKillDude(int nKillerSprite, spritetype *pSprite, DAMAGE_TYPE damageType,
                         aiGenDudeNewState(pSprite, &genDudeBurnGoto);
                         actHealDude(pXSprite, dudeInfo[55].startHealth, dudeInfo[55].startHealth);
                         if (pXSprite->burnTime <= 0) pXSprite->burnTime = 1200;
-                        gDudeExtra[pSprite->extra].at0 = (int)gFrameClock + 360;
+                        gDudeExtra[pSprite->extra].clock = (int)gFrameClock + 360;
                         return;
                     }
 
@@ -3357,7 +3357,7 @@ void actKillDude(int nKillerSprite, spritetype *pSprite, DAMAGE_TYPE damageType,
     case kDudeSpiderBrown:
         if (pSprite->owner != -1) {
             spritetype *pOwner = &sprite[actSpriteOwnerToSpriteId(pSprite)];
-            gDudeExtra[pOwner->extra].at6.u1.at4--;
+            gDudeExtra[pOwner->extra].stats.birthCounter--;
         }
         
         if (Chance(0x4000) && nSeq == 3) sfxPlay3DSound(pSprite, 1805, -1, 0);
@@ -3367,7 +3367,7 @@ void actKillDude(int nKillerSprite, spritetype *pSprite, DAMAGE_TYPE damageType,
     case kDudeSpiderRed:
         if (pSprite->owner != -1) {
             spritetype *pOwner = &sprite[actSpriteOwnerToSpriteId(pSprite)];
-            gDudeExtra[pOwner->extra].at6.u1.at4--;
+            gDudeExtra[pOwner->extra].stats.birthCounter--;
         }
         
         if (Chance(0x4000) && nSeq == 3) sfxPlay3DSound(pSprite, 1805, -1, 0);
@@ -3377,7 +3377,7 @@ void actKillDude(int nKillerSprite, spritetype *pSprite, DAMAGE_TYPE damageType,
     case kDudeSpiderBlack:
         if (pSprite->owner != -1) {
             spritetype *pOwner = &sprite[actSpriteOwnerToSpriteId(pSprite)];
-            gDudeExtra[pOwner->extra].at6.u1.at4--;
+            gDudeExtra[pOwner->extra].stats.birthCounter--;
         }
         
         if (Chance(0x4000) && nSeq == 3) sfxPlay3DSound(pSprite, 1805, -1, 0);

source/blood/src/ai.h
@@ -48,30 +48,21 @@ enum AI_SFX_PRIORITY {
 };
 
 
-struct DUDEEXTRA_at6_u1
+struct DUDEEXTRA_STATS
 {
-    int at0;
-    int at4;
-    char at8;
-};
-
-struct DUDEEXTRA_at6_u2
-{
-    int at0;
-    char at4;
+    union {
+        int thinkTime;
+        int birthCounter;
+    };
+    char active;
 };
 
 struct DUDEEXTRA
 {
-    int at0;
-    char at4;
-    AI_SFX_PRIORITY at5;
-    union
-    {
-        DUDEEXTRA_at6_u1 u1;
-        DUDEEXTRA_at6_u2 u2;
-    } at6;
-    //DUDEEXTRA_at6 at6;
+    int clock;
+    char teslaHit;
+    AI_SFX_PRIORITY sfx_priority;
+    DUDEEXTRA_STATS stats;
 };
 
 struct TARGETTRACK {

source/blood/src/aibat.cpp
@@ -90,12 +90,12 @@ static void thinkTarget(spritetype *pSprite, XSPRITE *pXSprite)
 {
     dassert(pSprite->type >= kDudeBase && pSprite->type < kDudeMax);
     DUDEINFO *pDudeInfo = getDudeInfo(pSprite->type);
-    DUDEEXTRA_at6_u1 *pDudeExtraE = &gDudeExtra[pSprite->extra].at6.u1;
-    if (pDudeExtraE->at8 && pDudeExtraE->at4 < 10)
-        pDudeExtraE->at4++;
-    else if (pDudeExtraE->at4 >= 10 && pDudeExtraE->at8)
+    DUDEEXTRA_STATS *pDudeExtraE = &gDudeExtra[pSprite->extra].stats;
+    if (pDudeExtraE->active && pDudeExtraE->thinkTime < 10)
+        pDudeExtraE->thinkTime++;
+    else if (pDudeExtraE->thinkTime >= 10 && pDudeExtraE->active)
     {
-        pDudeExtraE->at4 = 0;
+        pDudeExtraE->thinkTime = 0;
         pXSprite->goalAng += 256;
         POINT3D *pTarget = &baseSprite[pSprite->index];
         aiSetTarget(pXSprite, pTarget->x, pTarget->y, pTarget->z);
@@ -427,8 +427,8 @@ void MoveToCeil(spritetype *pSprite, XSPRITE *pXSprite)
     int nSector = pSprite->sectnum;
     if (z - pXSprite->targetZ < 0x1000)
     {
-        DUDEEXTRA_at6_u1 *pDudeExtraE = &gDudeExtra[pSprite->extra].at6.u1;
-        pDudeExtraE->at8 = 0;
+        DUDEEXTRA_STATS *pDudeExtraE = &gDudeExtra[pSprite->extra].stats;
+        pDudeExtraE->active = 0;
         pSprite->flags = 0;
         aiNewState(pSprite, pXSprite, &batIdle);
     }

source/blood/src/aiboneel.cpp
@@ -103,12 +103,12 @@ static void thinkTarget(spritetype *pSprite, XSPRITE *pXSprite)
 {
     dassert(pSprite->type >= kDudeBase && pSprite->type < kDudeMax);
     DUDEINFO *pDudeInfo = getDudeInfo(pSprite->type);
-    DUDEEXTRA_at6_u1 *pDudeExtraE = &gDudeExtra[pSprite->extra].at6.u1;
-    if (pDudeExtraE->at8 && pDudeExtraE->at4 < 10)
-        pDudeExtraE->at4++;
-    else if (pDudeExtraE->at4 >= 10 && pDudeExtraE->at8)
+    DUDEEXTRA_STATS *pDudeExtraE = &gDudeExtra[pSprite->extra].stats;
+    if (pDudeExtraE->active && pDudeExtraE->thinkTime < 10)
+        pDudeExtraE->thinkTime++;
+    else if (pDudeExtraE->thinkTime >= 10 && pDudeExtraE->active)
     {
-        pDudeExtraE->at4 = 0;
+        pDudeExtraE->thinkTime = 0;
         pXSprite->goalAng += 256;
         POINT3D *pTarget = &baseSprite[pSprite->index];
         aiSetTarget(pXSprite, pTarget->x, pTarget->y, pTarget->z);
@@ -136,13 +136,13 @@ static void thinkTarget(spritetype *pSprite, XSPRITE *pXSprite)
             int nDeltaAngle = ((getangle(dx,dy)+1024-pSprite->ang)&2047)-1024;
             if (nDist < pDudeInfo->seeDist && klabs(nDeltaAngle) <= pDudeInfo->periphery)
             {
-                pDudeExtraE->at4 = 0;
+                pDudeExtraE->thinkTime = 0;
                 aiSetTarget(pXSprite, pPlayer->nSprite);
                 aiActivateDude(pSprite, pXSprite);
             }
             else if (nDist < pDudeInfo->hearDist)
             {
-                pDudeExtraE->at4 = 0;
+                pDudeExtraE->thinkTime = 0;
                 aiSetTarget(pXSprite, x, y, z);
                 aiActivateDude(pSprite, pXSprite);
             }
@@ -432,8 +432,8 @@ void MoveToCeil(spritetype *pSprite, XSPRITE *pXSprite)
     int nSector = pSprite->sectnum;
     if (z - pXSprite->targetZ < 0x1000)
     {
-        DUDEEXTRA_at6_u1 *pDudeExtraE = &gDudeExtra[pSprite->extra].at6.u1;
-        pDudeExtraE->at8 = 0;
+        DUDEEXTRA_STATS *pDudeExtraE = &gDudeExtra[pSprite->extra].stats;
+        pDudeExtraE->active = 0;
         pSprite->flags = 0;
         aiNewState(pSprite, pXSprite, &eelIdle);
     }

source/blood/src/aicerber.cpp
@@ -275,10 +275,10 @@ static void thinkTarget(spritetype *pSprite, XSPRITE *pXSprite)
         return;
     }
     DUDEINFO *pDudeInfo = getDudeInfo(pSprite->type);
-    DUDEEXTRA_at6_u1 *pDudeExtraE = &gDudeExtra[pSprite->extra].at6.u1;
-    if (pDudeExtraE->at8 && pDudeExtraE->at4 < 10)
-        pDudeExtraE->at4++;
-    else if (pDudeExtraE->at4 >= 10 && pDudeExtraE->at8)
+    DUDEEXTRA_STATS *pDudeExtraE = &gDudeExtra[pSprite->extra].stats;
+    if (pDudeExtraE->active && pDudeExtraE->thinkTime < 10)
+        pDudeExtraE->thinkTime++;
+    else if (pDudeExtraE->thinkTime >= 10 && pDudeExtraE->active)
     {
         pXSprite->goalAng += 256;
         POINT3D *pTarget = &baseSprite[pSprite->index];
@@ -310,13 +310,13 @@ static void thinkTarget(spritetype *pSprite, XSPRITE *pXSprite)
             int nDeltaAngle = ((getangle(dx,dy)+1024-pSprite->ang)&2047)-1024;
             if (nDist < pDudeInfo->seeDist && klabs(nDeltaAngle) <= pDudeInfo->periphery)
             {
-                pDudeExtraE->at0 = 0;
+                pDudeExtraE->thinkTime = 0;
                 aiSetTarget(pXSprite, pPlayer->nSprite);
                 aiActivateDude(pSprite, pXSprite);
             }
             else if (nDist < pDudeInfo->hearDist)
             {
-                pDudeExtraE->at0 = 0;
+                pDudeExtraE->thinkTime = 0;
                 aiSetTarget(pXSprite, x, y, z);
                 aiActivateDude(pSprite, pXSprite);
             }

source/blood/src/aigarg.cpp
@@ -238,10 +238,10 @@ static void thinkTarget(spritetype *pSprite, XSPRITE *pXSprite)
         return;
     }
     DUDEINFO *pDudeInfo = getDudeInfo(pSprite->type);
-    DUDEEXTRA_at6_u1 *pDudeExtraE = &gDudeExtra[pSprite->extra].at6.u1;
-    if (pDudeExtraE->at8 && pDudeExtraE->at4 < 10)
-        pDudeExtraE->at4++;
-    else if (pDudeExtraE->at4 >= 10 && pDudeExtraE->at8)
+    DUDEEXTRA_STATS *pDudeExtraE = &gDudeExtra[pSprite->extra].stats;
+    if (pDudeExtraE->active && pDudeExtraE->thinkTime < 10)
+        pDudeExtraE->thinkTime++;
+    else if (pDudeExtraE->thinkTime >= 10 && pDudeExtraE->active)
     {
         pXSprite->goalAng += 256;
         POINT3D *pTarget = &baseSprite[pSprite->index];
@@ -270,13 +270,13 @@ static void thinkTarget(spritetype *pSprite, XSPRITE *pXSprite)
             int nDeltaAngle = ((getangle(dx,dy)+1024-pSprite->ang)&2047)-1024;
             if (nDist < pDudeInfo->seeDist && klabs(nDeltaAngle) <= pDudeInfo->periphery)
             {
-                pDudeExtraE->at4 = 0;
+                pDudeExtraE->thinkTime = 0;
                 aiSetTarget(pXSprite, pPlayer->nSprite);
                 aiActivateDude(pSprite, pXSprite);
             }
             else if (nDist < pDudeInfo->hearDist)
             {
-                pDudeExtraE->at4 = 0;
+                pDudeExtraE->thinkTime = 0;
                 aiSetTarget(pXSprite, x, y, z);
                 aiActivateDude(pSprite, pXSprite);
             }

tmyqlfpir/NotBlood

commit 3e6602b39964a23303b0b2491a018f9433404ca6

Reset thinkTime for kDudeModernCustomDude
Fixes error introduced in commit https://github.com/nukeykt/NBlood/commit/08c35968b11f04f49d42477f29c3cbbd06ac4cd7

source/blood/src/aiunicult.cpp
@@ -2192,7 +2192,7 @@ void aiGenDudeInitSprite(spritetype* pSprite, XSPRITE* pXSprite) {
     switch (pSprite->type) {
         case kDudeModernCustom: {
             DUDEEXTRA_STATS* pDudeExtraE = &gDudeExtra[pSprite->extra].stats;
-            pDudeExtraE->active = 0;
+            pDudeExtraE->active = pDudeExtraE->thinkTime = 0;
             aiGenDudeNewState(pSprite, &genDudeIdleL);
             break;
         }