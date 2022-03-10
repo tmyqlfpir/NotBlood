f74ee02c930df7135a3c70bc6e08b1aceb6925c6
‮ <38839485+carnivoroussociety@users.noreply.github.com>
2022-03-11 01:58:47 +1000

    Improve weapon select bar behavior
    Match next weapon logic as player switching

--- source/blood/src/view.cpp
+++ source/blood/src/view.cpp
@@ -1508,8 +1508,23 @@ void viewDrawWeaponSelect(PLAYER* pPlayer, XSPRITE *pXSprite)
     }
 
     const int bakCurWeapon = pPlayer->curWeapon;
-    if (pPlayer->curWeapon == kWeaponNone) // if we're switching between weapons, use the next weapon value
+    while (pPlayer->curWeapon == kWeaponNone) // if we're switching between weapons, use the next weapon value or find a valid weapon
+    {
         pPlayer->curWeapon = pPlayer->input.newWeapon;
+        if (WeaponIsEquipable(pPlayer, pPlayer->curWeapon))
+            break;
+        pPlayer->curWeapon = pPlayer->nextWeapon;
+        if (WeaponIsEquipable(pPlayer, pPlayer->curWeapon))
+            break;
+        pPlayer->curWeapon = WeaponFindLoaded(pPlayer, NULL);
+        if (WeaponIsEquipable(pPlayer, pPlayer->curWeapon))
+            break;
+        pPlayer->curWeapon = pPlayer->weaponAmmo;
+        if (WeaponIsEquipable(pPlayer, pPlayer->curWeapon))
+            break;
+        pPlayer->curWeapon = kWeaponPitchfork;
+        break;
+    }
     const char weaponPrev = ClipRange(WeaponFindNext(pPlayer, NULL, 0), 1, 12);
     const char weaponCur = ClipRange(pPlayer->curWeapon, 0, 13);
     const char weaponNext = ClipRange(WeaponFindNext(pPlayer, NULL, 1), 1, 12);
