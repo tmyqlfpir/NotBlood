cd459a9d4a4284ea4331136bae28ee5060d20727
‮ <38839485+carnivoroussociety@users.noreply.github.com>
2022-03-14 01:47:31 +1000

    Add lift support for wall aligned sprites

--- source/blood/src/triggers.cpp
+++ source/blood/src/triggers.cpp
@@ -1034,10 +1034,33 @@ void ZTranslateSector(int nSector, XSECTOR *pXSector, int a3, int a4)
                 viewBackupSpriteLoc(nSprite, pSprite);
                 pSprite->z += pSector->floorz-oldZ;
             }
-            else if (gGameOptions.bSectorBehavior && !VanillaMode() && ((pSprite->cstat&CSTAT_SPRITE_ALIGNMENT_MASK) == CSTAT_SPRITE_ALIGNMENT_FLOOR)) // if floor aligned sprite
+            else if (gGameOptions.bSectorBehavior && !VanillaMode()) // move floor/wall aligned sprites along sector
             {
-                viewBackupSpriteLoc(nSprite, pSprite);
-                pSprite->z += pSector->floorz-oldZ;
+                char bDraggable = 0;
+                if ((oldZ <= bottom) && ((pSprite->cstat&CSTAT_SPRITE_ALIGNMENT_MASK) != CSTAT_SPRITE_ALIGNMENT_WALL)) // if sprite is sitting on the floor (and not wall aligned)
+                    bDraggable = 1;
+                else if ((pXSector->onCeilZ != pXSector->offCeilZ) && (sector[nSector].wallnum > 0) && ((pSprite->cstat&CSTAT_SPRITE_ALIGNMENT_MASK) == CSTAT_SPRITE_ALIGNMENT_WALL)) // if ceiling is also moving (e.g: elevator), adjust wall aligned sprites
+                {
+                    double nDist = DBL_MAX;
+                    const int nStartWall = sector[nSector].wallptr;
+                    const int nEndWall = nStartWall + sector[nSector].wallnum;
+                    int nFoundWall = 0;
+                    for (int nWall = nStartWall; nWall < nEndWall; nWall++) // because elevators have an entrance, we need to only move sprites aligned to the elevator interior walls
+                    {
+                        const double nDistCurWall = SquareDistToWall(pSprite->x, pSprite->y, &wall[nWall]); // find closest wall to sprite
+                        if (nDistCurWall < nDist)
+                        {
+                            nDist = nDistCurWall;
+                            nFoundWall = nWall;
+                        }
+                    }
+                    bDraggable = wall[nFoundWall].nextsector < 0; // if nearest wall is linked to a sector, do not drag (outside of elevator)
+                }
+                if (bDraggable)
+                {
+                    viewBackupSpriteLoc(nSprite, pSprite);
+                    pSprite->z += pSector->floorz-oldZ;
+                }
             }
         }
     }
