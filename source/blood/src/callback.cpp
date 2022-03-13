9849630a1c92ee115635b9b008928c9d1112c63f
‮ <38839485+carnivoroussociety@users.noreply.github.com>
2022-03-13 20:39:49 +1000

    Allow FX_36 to collide with fake floors/bridges
    Only clamp to sector if within one meter of sector floor

--- source/blood/src/callback.cpp
+++ source/blood/src/callback.cpp
@@ -469,13 +469,17 @@ void fxBloodBits(int nSprite) // 14
             pFX->ang = nAngle;
             if (gGameOptions.bSectorBehavior && !VanillaMode())
             {
-                pFX->z = getflorzofslope(pFX->sectnum, pFX->x, pFX->y); // get more accurate z position of sector
-                if ((sector[pFX->sectnum].floorstat&2) && (sector[pFX->sectnum].floorheinum != 0)) // align sprite to slope
+                floorZ = getflorzofslope(pFX->sectnum, pFX->x, pFX->y); // get more accurate z position of sector
+                if (klabs(pFX->z-floorZ) <= (32<<8)) // if sprite is less than one meter from floor, set to sector height and test for slope
                 {
-                    walltype *pWall1 = &wall[sector[pFX->sectnum].wallptr];
-                    walltype *pWall2 = &wall[pWall1->point2];
-                    spriteSetSlope(pFX->index, sector[pFX->sectnum].floorheinum);
-                    pFX->ang = getangle(pWall2->x-pWall1->x, pWall2->y-pWall1->y)+kAng270;
+                    pFX->z = floorZ;
+                    if ((sector[pFX->sectnum].floorstat&2) && (sector[pFX->sectnum].floorheinum != 0)) // align sprite to slope
+                    {
+                        walltype *pWall1 = &wall[sector[pFX->sectnum].wallptr];
+                        walltype *pWall2 = &wall[pWall1->point2];
+                        spriteSetSlope(pFX->index, sector[pFX->sectnum].floorheinum);
+                        pFX->ang = getangle(pWall2->x-pWall1->x, pWall2->y-pWall1->y)+kAng270;
+                    }
                 }
             }
         }
