e42785357b25202fcad931e30d41c67c6b233ce2
‮ <38839485+carnivoroussociety@users.noreply.github.com>
2022-03-16 13:15:49 +1000

    Update listening position for nnexts teleport

--- source/blood/src/nnexts.cpp
+++ source/blood/src/nnexts.cpp
@@ -2823,6 +2823,10 @@ void useTeleportTarget(XSPRITE* pXSource, spritetype* pSprite) {
         playerResetInertia(pPlayer);
         if (pXSource->data2 == 1)
             pPlayer->zViewVel = pPlayer->zWeaponVel = 0;
+        if (pPlayer == gMe) { // if player is listener, update ear position/reset ear velocity so audio pitch of surrounding sfx does not freak out when teleporting player
+            sfxUpdateListenerPos();
+            sfxUpdateListenerVel(true);
+        }
     }
 
 }
