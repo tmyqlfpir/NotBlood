6917845e5cffb94fb7cf1e51d6291e67f523bd1c
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2022-04-03 12:43:16 +1000

    Use non-fixed units if weapon interpolation is off
    Matches original logic better than previous method

--- source/blood/src/view.cpp
+++ source/blood/src/view.cpp
@@ -4208,19 +4208,20 @@ RORHACK:
                     defaultHoriz += 19;
                 rotatesprite(160<<16, defaultHoriz<<16, 65536, 0, kCrosshairTile, 0, g_isAlterDefaultCrosshair ? CROSSHAIR_PAL : 0, 2, gViewX0, gViewY0, gViewX1, gViewY1);
             }
-            if (!gWeaponInterpolate) // if position interpolate weapon is off, quantize the weapon positions
-            {
-                v4c >>= 8;
-                v4c <<= 8;
-                v48 >>= 8;
-                v48 <<= 8;
-                zDelta >>= 7;
-                zDelta <<= 7;
-            }
             if (!gWeaponHBobbing) // disable weapon sway
                 v4c = 0;
-            cX = (v4c<<8)+(160<<16);
-            cY = (v48<<8)+(220<<16)+(zDelta<<9);
+            if (!gWeaponInterpolate) // if position interpolate weapon is off, quantize the weapon positions
+            {
+                cX = (v4c>>8)+160;
+                cY = (v48>>8)+220+(zDelta>>7);
+                cY <<= 16;
+                cX <<= 16;
+            }
+            else // default
+            {
+                cX = (v4c<<8)+(160<<16);
+                cY = (v48<<8)+(220<<16)+(zDelta<<9);
+            }
             int nShade = sector[nSectnum].floorshade; int nPalette = 0;
             if (sector[gView->pSprite->sectnum].extra > 0) {
                 sectortype *pSector = &sector[gView->pSprite->sectnum];
