c2a0f2934952f8604f2127293c9f090b8e7248fa
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2022-04-13 16:55:49 +1000

    Scale background hud tiles to match hud pixel size
    Use vanilla background tiles in vanilla mode (tile ID 230)

--- source/blood/src/view.cpp
+++ source/blood/src/view.cpp
@@ -1119,7 +1119,7 @@ void viewDrawText(int nFont, const char *pString, int x, int y, int nShade, int 
     }
 }
 
-void viewTileSprite(int nTile, int nShade, int nPalette, int x1, int y1, int x2, int y2)
+void viewTileSprite(int nTile, int nShade, int nPalette, int x1, int y1, int x2, int y2, int nWidth, int nHeight, int nScale)
 {
     Rect rect1 = Rect(x1, y1, x2, y2);
     Rect rect2 = Rect(0, 0, xdim, ydim);
@@ -1129,15 +1129,15 @@ void viewTileSprite(int nTile, int nShade, int nPalette, int x1, int y1, int x2,
         return;
 
     dassert(nTile >= 0 && nTile < kMaxTiles);
-    int width = tilesiz[nTile].x;
-    int height = tilesiz[nTile].y;
+    int width = !nWidth ? tilesiz[nTile].x : nWidth;
+    int height = !nHeight ? tilesiz[nTile].y : nHeight;
     int bx1 = DecBy(rect1.x0+1, width);
     int by1 = DecBy(rect1.y0+1, height);
     int bx2 = IncBy(rect1.x1-1, width);
     int by2 = IncBy(rect1.y1-1, height);
     for (int x = bx1; x < bx2; x += width)
         for (int y = by1; y < by2; y += height)
-            rotatesprite(x<<16, y<<16, 65536, 0, nTile, nShade, nPalette, 64+16+8, x1, y1, x2-1, y2-1);
+            rotatesprite(x<<16, y<<16, nScale, 0, nTile, nShade, nPalette, 64+16+8, x1, y1, x2-1, y2-1);
 }
 
 void InitStatusBar(void)
@@ -2257,7 +2257,7 @@ void viewResizeView(int size)
 }
 
 #define kBackTile 253
-//#define kBackTileVanilla 230
+#define kBackTileVanilla 230
 
 void UpdateFrame(void)
 {
@@ -2270,15 +2270,33 @@ void UpdateFrame(void)
             nPalette = 10;
     }
 
-    viewTileSprite(kBackTile, 0, nPalette, 0, 0, xdim, gViewY0-3);
-    viewTileSprite(kBackTile, 0, nPalette, 0, gViewY1+4, xdim, ydim);
-    viewTileSprite(kBackTile, 0, nPalette, 0, gViewY0-3, gViewX0-3, gViewY1+4);
-    viewTileSprite(kBackTile, 0, nPalette, gViewX1+4, gViewY0-3, xdim, gViewY1+4);
+    const int nTile = !VanillaMode() ? kBackTile : kBackTileVanilla;
+    int nScale = 65536;
+    int nWidth = 0, nHeight = 0;
+    if (!VanillaMode()) // scale background tiles to match hud pixel density scale
+    {
+        nWidth = tilesiz[nTile].x;
+        nHeight = tilesiz[nTile].y;
+        if (nWidth == nHeight) // if tile is perfectly square
+        {
+            nWidth = nHeight = mulscale16(nWidth<<16, xscalecorrect)>>16;
+            nScale = mulscale16(nScale, xscalecorrect);
+        }
+        else // unexpected non-square tile, don't scale
+        {
+            nWidth = nHeight = 0;
+        }
+    }
 
-    viewTileSprite(kBackTile, 20, nPalette, gViewX0-3, gViewY0-3, gViewX0, gViewY1+1);
-    viewTileSprite(kBackTile, 20, nPalette, gViewX0, gViewY0-3, gViewX1+4, gViewY0);
-    viewTileSprite(kBackTile, 10, nPalette+1, gViewX1+1, gViewY0, gViewX1+4, gViewY1+4);
-    viewTileSprite(kBackTile, 10, nPalette+1, gViewX0-3, gViewY1+1, gViewX1+1, gViewY1+4);
+    viewTileSprite(nTile, 0, nPalette, 0, 0, xdim, gViewY0-3, nWidth, nHeight, nScale);
+    viewTileSprite(nTile, 0, nPalette, 0, gViewY1+4, xdim, ydim, nWidth, nHeight, nScale);
+    viewTileSprite(nTile, 0, nPalette, 0, gViewY0-3, gViewX0-3, gViewY1+4, nWidth, nHeight, nScale);
+    viewTileSprite(nTile, 0, nPalette, gViewX1+4, gViewY0-3, xdim, gViewY1+4, nWidth, nHeight, nScale);
+
+    viewTileSprite(nTile, 20, nPalette, gViewX0-3, gViewY0-3, gViewX0, gViewY1+1, nWidth, nHeight, nScale);
+    viewTileSprite(nTile, 20, nPalette, gViewX0, gViewY0-3, gViewX1+4, gViewY0, nWidth, nHeight, nScale);
+    viewTileSprite(nTile, 10, nPalette+1, gViewX1+1, gViewY0, gViewX1+4, gViewY1+4, nWidth, nHeight, nScale);
+    viewTileSprite(nTile, 10, nPalette+1, gViewX0-3, gViewY1+1, gViewX1+1, gViewY1+4, nWidth, nHeight, nScale);
 }
 
 void viewDrawInterface(ClockTicks arg)
