76570bce283ed015bdaf01e1048301ea344592eb
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2022-05-04 18:57:34 +1000

    Range clamp SoundSpeed/EarAng before converting

--- source/blood/src/sfx.cpp
+++ source/blood/src/sfx.cpp
@@ -557,7 +557,7 @@ static void sfxUpdateSpeedOfSound(void)
 {
     if (gSoundSpeed != oldSoundSpeed) // if speed of sound setting has been changed, convert real world meters to build engine units
     {
-        oldSoundSpeed = gSoundSpeed;
+        oldSoundSpeed = gSoundSpeed = ClipRange(gSoundEarAng, 10, 1000);
         nSoundSpeed = (int)(((32<<4) * gSoundSpeed) / kTicsPerSec);
     }
 }
@@ -566,7 +566,7 @@ static void sfxUpdateEarAng(void)
 {
     if (gSoundEarAng != oldEarAng) // if ear angle setting has been changed, convert degrees to build engine degrees
     {
-        oldEarAng = gSoundEarAng;
+        oldEarAng = gSoundEarAng = ClipRange(gSoundEarAng, 15, 90);
         nEarAng = (int)(gSoundEarAng * (kAng360 / 360.f));
     }
 }
