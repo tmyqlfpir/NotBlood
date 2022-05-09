b2d4d65057d72ea4d5b79766bca256388bad1471
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2022-05-09 16:58:30 +1000

    Safely delete banned sprites on spawn trigger

--- source/blood/src/triggers.cpp
+++ source/blood/src/triggers.cpp
@@ -505,6 +505,9 @@ void OperateSprite(int nSprite, XSPRITE *pXSprite, EVENT event)
                         dbRandomizerModeScale(pSpawn, pXSpawn);
                 }
                 if (dbIsBannedDude(pSpawn, pXSpawn)) { // if spawned sprite is banned, remove sprite
+                    evKill(pSpawn->index, 3);
+                    if (sprite[pSpawn->index].extra > 0)
+                        seqKill(3, sprite[pSpawn->index].extra);
                     DeleteSprite(pSpawn->index);
                     break;
                 }
