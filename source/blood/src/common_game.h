22bb7e672937667ea3f351be76313e6de59815b2
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2023-01-07 23:22:17 +1000

    Update game save version

--- source/blood/src/common_game.h
+++ source/blood/src/common_game.h
@@ -41,7 +41,7 @@ extern int g_useCwd;
 #define BLOODWIDESCREENDEF "blood_widescreen.def"
 #define NOTBLOODDEF "notblood.def"
 
-#define BYTEVERSION 116
+#define BYTEVERSION 117
 #define EXEVERSION 101
 
 void _SetErrorLoc(const char *pzFile, int nLine);
