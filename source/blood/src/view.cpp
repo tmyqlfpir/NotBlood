tmyqlfpir/NotBlood

commit 6395beb450ca4750c65b8e9dfe360170ee994d86

Sanity check viewing sector when disconnecting mp

source/blood/src/view.cpp
@@ -4216,7 +4216,7 @@ void viewDrawScreen(void)
             CalcPosition(gView->pSprite, (int*)&cX, (int*)&cY, (int*)&cZ, &nSectnum, fix16_to_int(cA), q16horiz);
         }
         const char bLink = CheckLink((int*)&cX, (int*)&cY, (int*)&cZ, &nSectnum);
-        if (!bLink && gViewInterpolate && !VanillaMode()) // double check current sector for interpolated movement (fixes ROR glitch such as E3M5's scanning room doorway)
+        if (!bLink && gViewInterpolate && !VanillaMode() && (nSectnum != 1)) // double check current sector for interpolated movement (fixes ROR glitch such as E3M5's scanning room doorway)
         {
             int nFoundSect = nSectnum;
             if (FindSector(cX, cY, cZ, &nFoundSect) && (nFoundSect != nSectnum) && AreSectorsNeighbors(nSectnum, nFoundSect, 1)) // if newly found sector is connected to current sector, set as view sector

commit 066454f707251e11d95c4fb90439a02522b97e31

Fix bug introduced in commit 6395beb450ca4750c65b8e9dfe360170ee994d86

source/blood/src/view.cpp
@@ -4216,7 +4216,7 @@ void viewDrawScreen(void)
             CalcPosition(gView->pSprite, (int*)&cX, (int*)&cY, (int*)&cZ, &nSectnum, fix16_to_int(cA), q16horiz);
         }
         const char bLink = CheckLink((int*)&cX, (int*)&cY, (int*)&cZ, &nSectnum);
-        if (!bLink && gViewInterpolate && !VanillaMode() && (nSectnum != 1)) // double check current sector for interpolated movement (fixes ROR glitch such as E3M5's scanning room doorway)
+        if (!bLink && gViewInterpolate && !VanillaMode() && (nSectnum != -1)) // double check current sector for interpolated movement (fixes ROR glitch such as E3M5's scanning room doorway)
         {
             int nFoundSect = nSectnum;
             if (FindSector(cX, cY, cZ, &nFoundSect) && (nFoundSect != nSectnum) && AreSectorsNeighbors(nSectnum, nFoundSect, 1)) // if newly found sector is connected to current sector, set as view sector