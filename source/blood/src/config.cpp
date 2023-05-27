da2f97981cb78191fcd5ff8e934fc4b715621b31
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2023-05-27 14:36:12 +1000

    Update config.cpp

--- source/blood/src/config.cpp
+++ source/blood/src/config.cpp
@@ -403,7 +403,7 @@ void CONFIG_SetDefaults(void)
     gCustomPaletteInvert = 0;
     //ud.config.ShowWeapons     = 0;
     SoundToggle     = 1;
-    CDAudioToggle = 1;
+    CDAudioToggle = 0;
     DopplerToggle = 1;
     MusicDevice = ASS_SF2;
     Bstrcpy(SF2_BankFile, "notblood.sf2");
