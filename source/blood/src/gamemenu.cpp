42f368e7359efd079956f19e6c5eb29a46f51180
tmyqlfpir <80724828+tmyqlfpir@users.noreply.github.com>
2023-08-15 09:04:24 +1000

    Update gamemenu.cpp

--- source/blood/src/gamemenu.cpp
+++ source/blood/src/gamemenu.cpp
@@ -39,7 +39,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 CMenuTextMgr gMenuTextMgr;
 CGameMenuMgr gGameMenuMgr;
-static CGameMenuItemZEdit *pGameMenuItemZEdit = NULL;
 
 extern CGameMenuItemQAV itemBloodQAV;
 static CGameMenuItemZEdit *pGameMenuItemZEdit = NULL;
