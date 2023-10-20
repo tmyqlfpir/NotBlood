tmyqlfpir/NotBlood

commit a95795a6b30af492611f0985c708136a67f5eb00

Fix bug where remote profile was not updated on new level
Use gProfileNet struct when starting level - this should always be the latest profile update, regardless in single-player or multiplayer

source/blood/src/blood.cpp
@@ -824,7 +824,7 @@ void StartLevel(GAMEOPTIONS *gameOptions)
         }
         else if ((gGameOptions.nGameType == kGameTypeTeams) && !VanillaMode()) // if ctf mode and went to next level, reset scores
             playerResetScores(i);
-        gProfileNet[i] = gProfile[i];
+        gProfile[i] = gProfileNet[i]; // gProfileNet should always be the latest profile from remote players
         playerStart(i, 1);
     }
     playerInitRoundCheck();

source/blood/src/demo.cpp
@@ -558,7 +558,6 @@ _DEMOPLAYBACK:
         {
             if (!v4)
             {
-                int nAutoAim = 1;
                 viewResizeView(gViewSize);
                 viewSetMessage("");
                 gNetPlayers = atf.nNetPlayers;
@@ -574,6 +573,14 @@ _DEMOPLAYBACK:
                 gGameOptions.uGameFlags &= ~kGameFlagContinuing; // don't let demo attempt to load player health from gHealthTemp
                 playerSetSkill(gGameOptions.nDifficulty); // set skill to same value as current difficulty
                 for (int i = 0; i < kMaxPlayers; i++)
+                {
+                    gProfile[i].nAutoAim = 1;
+                    gProfile[i].nWeaponSwitch = 1;
+                    gProfile[i].bWeaponFastSwitch = 0;
+                    gProfile[i].nWeaponHBobbing = 1;
+                    gProfileNet[i] = gProfile[i];
+                }
+                for (int i = 0; i < kMaxPlayers; i++)
                     playerInit(i, 0);
                 StartLevel(&gGameOptions);
                 if (gDemoRunValidation) // if we're executing validation test
@@ -585,19 +592,11 @@ _DEMOPLAYBACK:
                         if (!pCurrentDemo || Bstrcasecmp(pCurrentDemo->zName, gDemoValidate[index].zName)) // demo name does not match, skip
                             continue;
                         pValidateInfo = &gDemoValidate[index]; // found demo's verified results, set as validate info
-                        nAutoAim = pValidateInfo->nAutoAim; // assign auto aim setting from validate info
+                        gProfile[myconnectindex].nAutoAim = pValidateInfo->nAutoAim; // assign auto aim setting from validate info
                         break;
                     }
                     if (!pValidateInfo) // run newly added verify demos at a slower speed for visual verification
                         timerInit(CLOCKTICKSPERSECOND*5);
-                }
-                for (int i = 0; i < kMaxPlayers; i++)
-                {
-                    gProfile[i].nAutoAim = nAutoAim;
-                    gProfile[i].nWeaponSwitch = 1;
-                    gProfile[i].bWeaponFastSwitch = 0;
-                    gProfile[i].nWeaponHBobbing = 1;
-                    gProfileNet[i] = gProfile[i];
                 }
             }
             ready2send = 0;

commit 8086339684565e7b977f267c30ec622925d935ca

Fix player damage bug introduced in commit a95795a6b30af492611f0985c708136a67f5eb00

source/blood/src/blood.cpp
@@ -811,6 +811,8 @@ void StartLevel(GAMEOPTIONS *gameOptions)
     evInit();
     for (int i = connecthead; i >= 0; i = connectpoint2[i])
     {
+        if ((numplayers > 1) || (gGameOptions.nGameType != kGameTypeSinglePlayer))
+            gProfile[i] = gProfileNet[i]; // gProfileNet should always be the latest profile from remote players
         if (!(gameOptions->uGameFlags&kGameFlagContinuing)) // if new game
         {
             if (numplayers == 1)
@@ -824,7 +826,6 @@ void StartLevel(GAMEOPTIONS *gameOptions)
         }
         else if ((gGameOptions.nGameType == kGameTypeTeams) && !VanillaMode()) // if ctf mode and went to next level, reset scores
             playerResetScores(i);
-        gProfile[i] = gProfileNet[i]; // gProfileNet should always be the latest profile from remote players
         playerStart(i, 1);
     }
     playerInitRoundCheck();

source/blood/src/player.cpp
@@ -918,7 +918,7 @@ void playerStart(int nPlayer, int bNewLevel)
     // this is used to safely update profiles while in a network multiplayer session, for example...
     // if a player updated their autoaim settings while facing an enemy, it would cause a game desync thanks to the autoaim target changing for local player before the gProfile update packet has been sent to the other clients
     // by tunneling all mid-session gProfile updates to gProfileNet it'll allow all clients to update the current player's settings at the same tick, which is on spawn (this ensures everybody stays synced)
-    if ((numplayers > 1) && (gGameOptions.nGameType != kGameTypeSinglePlayer))
+    if ((numplayers > 1) || (gGameOptions.nGameType != kGameTypeSinglePlayer))
         gProfile[nPlayer] = gProfileNet[nPlayer];
 
     playerResetTeamId(nPlayer, bNewLevel);

source/blood/src/player.h
@@ -392,7 +392,7 @@ inline void playerSetSkill(int nSkill)
 {
     nSkill = ClipRange(nSkill, 0, 4);
     for (int i = 0; i < kMaxPlayers; i++) // set all player's skill setting
-        gProfile[i].skill = nSkill;
+        gProfile[i].skill = gProfileNet[i].skill = nSkill;
 }
 
 #define kFlagBluePal 10 // 10: dark blue