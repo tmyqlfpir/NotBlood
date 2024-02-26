tmyqlfpir/NotBlood

commit fdca60783628de6a83de2eafaaf7e53476151cbc

Add mirror mode setting for multiplayer

source/blood/src/blood.cpp
@@ -612,6 +612,7 @@ void StartLevel(GAMEOPTIONS *gameOptions)
         gDemo.Close();
     netWaitForEveryone(0);
     VanillaModeUpdate();
+    r_mirrormodelock = 0;
     if (gGameOptions.nGameType == kGameTypeSinglePlayer)
     {
         if (!(gGameOptions.uGameFlags&kGameFlagContinuing))
@@ -705,6 +706,12 @@ void StartLevel(GAMEOPTIONS *gameOptions)
         gRedFlagDropped = false;
         gView = gMe;
         gViewIndex = myconnectindex;
+        r_mirrormodelock = 1;
+        r_mirrormode = 0;
+        if (gGameOptions.uNetGameFlags&kNetGameFlagMirrorHoriz)
+            r_mirrormode |= 1;
+        if (gGameOptions.uNetGameFlags&kNetGameFlagMirrorVert)
+            r_mirrormode |= 2;
     }
     if (gameOptions->uGameFlags&kGameFlagContinuing) // if episode is in progress, remember player stats
     {

source/blood/src/menu.cpp
@@ -241,6 +241,13 @@ const char *zLengthStrings[] =
     "Frags",
 };
 
+const char *pzMirrorModeStrings[] = {
+    "OFF",
+    "HORIZONTAL",
+    "VERTICAL",
+    "HORIZONTAL+VERTICAL"
+};
+
 const char *zDiffStrings[] =
 {
     "STILL KICKING",
@@ -505,9 +512,12 @@ CGameMenuItemZCycle itemNetGameCycleItemWeapon("ITEM/WEAPON SETTING:", 3, 66, 11
 CGameMenuItemZBool itemNetGameBoolAutoTeams("AUTO TEAMS:", 3, 66, 105, 180, true, 0, NULL, NULL);
 CGameMenuItemZBool itemNetGameBoolTeamColors("TEAM COLORS:", 3, 66, 115, 180, true, 0, NULL, NULL);
 CGameMenuItemZCycle itemNetGameCycleSpawnLocation("SPAWN AREA:", 3, 66, 125, 180, 0, 0, zRespawnStrings, ARRAY_SSIZE(zRespawnStrings), 0);
-CGameMenuItemZCycle itemNetGameCycleShowWeaponsOverride("ENEMY WEAPONS:", 3, 66, 135, 180, 0, 0, zShowWeapon, ARRAY_SSIZE(zShowWeapon), 0);
 CGameMenuItemZCycle itemNetGameCycleSpawnProtection("SPAWN PROTECTION:", 3, 66, 145, 180, 0, 0, zSpawnProtectStrings, ARRAY_SSIZE(zSpawnProtectStrings), 0);
 CGameMenuItemZCycle itemNetGameCycleSpawnWeapon("SPAWN WITH WEAPON:", 3, 66, 155, 180, 0, SetNetGameMode, zSpawnWeaponStrings, ARRAY_SSIZE(zSpawnWeaponStrings), 0);
+CGameMenuItemZCycle itemNetGameCycleShowWeaponsOverride("ENEMY WEAPONS:", 3, 66, 134, 180, 0, 0, zShowWeapon, ARRAY_SSIZE(zShowWeapon), 0);
+CGameMenuItemZCycle itemNetGameCycleSpawnProtection("SPAWN PROTECTION:", 3, 66, 143, 180, 0, 0, zSpawnProtectStrings, ARRAY_SSIZE(zSpawnProtectStrings), 0);
+CGameMenuItemZCycle itemNetGameCycleSpawnWeapon("SPAWN WITH WEAPON:", 3, 66, 152, 180, 0, SetNetGameMode, zSpawnWeaponStrings, ARRAY_SSIZE(zSpawnWeaponStrings), 0);
+CGameMenuItemZCycle itemNetGameCycleMirrorModeOverride("MIRROR MODE:", 3, 66, 161, 180, 0, NULL, pzMirrorModeStrings, ARRAY_SSIZE(pzMirrorModeStrings), 0);
 
 CGameMenuItemTitle itemNetMonsterTitle("MONSTERS", 1, 160, 20, 2038);
 CGameMenuItemZCycle itemNetMonsterSettings("MONSTERS:", 3, 66, 40, 180, 0, SetNetMonsterMenu, zMonsterStrings, ARRAY_SSIZE(zMonsterStrings), 0);
@@ -659,13 +669,6 @@ const char *pzWeaponInterpolateStrings[] = {
     "OFF",
     "ONLY SWAYING",
     "ALL ANIMATION"
-};
-
-const char *pzMirrorModeStrings[] = {
-    "OFF",
-    "HORIZONTAL",
-    "VERTICAL",
-    "HORIZONTAL+VERTICAL"
 };
 
 void SetAutoAim(CGameMenuItemZCycle *pItem);
@@ -1433,6 +1436,7 @@ void SetupNetStartMenu(void)
     menuNetworkGameMode.Add(&itemNetGameCycleShowWeaponsOverride, false);
     menuNetworkGameMode.Add(&itemNetGameCycleSpawnProtection, false);
     menuNetworkGameMode.Add(&itemNetGameCycleSpawnWeapon, false);
+    menuNetworkGameMode.Add(&itemNetGameCycleMirrorModeOverride, false);
     menuNetworkGameMode.Add(&itemBloodQAV, false);
     itemNetGameBoolExit.tooltip_pzTextUpper = "Toggle level exit switch functionality";
     itemNetGameBoolTeleFrag.tooltip_pzTextUpper = "Toggle telefrags kills";
@@ -1443,6 +1447,8 @@ void SetupNetStartMenu(void)
     itemNetGameCycleSpawnLocation.tooltip_pzTextUpper = "Set spawn location behavior";
     itemNetGameCycleShowWeaponsOverride.tooltip_pzTextUpper = "Set global setting for show weapons option";
     itemNetGameCycleShowWeaponsOverride.tooltip_pzTextLower = "(This is applied to all players in round)";
+    itemNetGameCycleMirrorModeOverride.tooltip_pzTextUpper = "Set global setting for mirror mode";
+    itemNetGameCycleMirrorModeOverride.tooltip_pzTextLower = "(This is applied to all players in round)";
 
     menuNetworkGameMonsters.Add(&itemNetMonsterTitle, false);
     menuNetworkGameMonsters.Add(&itemNetMonsterSettings, true);
@@ -1885,6 +1891,7 @@ void SetupOptionsMenu(void)
     menuOptionsDisplayView.Add(&itemOptionsDisplayViewMirrorMode, false);
     menuOptionsDisplayView.Add(&itemOptionsDisplayViewBoolSlowRoomFlicker, false);
     menuOptionsDisplayView.Add(&itemBloodQAV, false);
+    itemOptionsDisplayViewMirrorMode.bDisableForNet = 1;
     itemOptionsDisplayViewBoolSlopeTilting.tooltip_pzTextUpper = "";
     itemOptionsDisplayViewBoolSlopeTilting.tooltip_pzTextLower = "Tilt view when looking towards slope";
     itemOptionsDisplayViewWeaponInterpolation.tooltip_pzTextUpper = "";
@@ -2479,7 +2486,10 @@ void SetLevelCompleteTime(CGameMenuItemZBool *pItem)
 
 void SetMirrorMode(CGameMenuItemZCycle *pItem)
 {
-    r_mirrormode = pItem->m_nFocus % ARRAY_SSIZE(pzMirrorModeStrings);
+    if (pItem == NULL)
+        pItem = &itemOptionsDisplayViewMirrorMode;
+    if (!r_mirrormodelock)
+        r_mirrormode = pItem->m_nFocus % ARRAY_SSIZE(pzMirrorModeStrings);
 }
 
 void SetSlowRoomFlicker(CGameMenuItemZBool *pItem)
@@ -4081,6 +4091,10 @@ void StartNetGame(CGameMenuItemChain *pItem)
         gPacketStartGame.uNetGameFlags |= itemNetGameCycleSpawnLocation.m_nFocus == 1 ? kNetGameFlagSpawnSmart : kNetGameFlagSpawnDist;
     if (itemNetGameCycleShowWeaponsOverride.m_nFocus > 0)
         gPacketStartGame.uNetGameFlags |= itemNetGameCycleShowWeaponsOverride.m_nFocus == 1 ? kNetGameFlagHideWeaponsCloak : kNetGameFlagHideWeaponsAlways;
+    if (itemNetGameCycleMirrorModeOverride.m_nFocus & 1)
+        gPacketStartGame.uNetGameFlags |= kNetGameFlagMirrorHoriz;
+    if (itemNetGameCycleMirrorModeOverride.m_nFocus & 2)
+        gPacketStartGame.uNetGameFlags |= kNetGameFlagMirrorVert;
     gPacketStartGame.episodeId = itemNetStart2.m_nFocus;
     gPacketStartGame.levelId = itemNetStart3.m_nFocus;
     gPacketStartGame.difficulty = itemNetStart4.m_nFocus;

source/blood/src/network.cpp
@@ -175,6 +175,8 @@ void netResetToSinglePlayer(void)
     gGameMenuMgr.Deactivate();
     gNetNotifyProfileUpdate = false;
     gPlayerRoundEnding = 0;
+    r_mirrormodelock = 0;
+    SetMirrorMode(NULL); // restore single-player setting for mirror mode
 }
 
 void netSendPacket(int nDest, char *pBuffer, int nSize)

source/blood/src/weapon.cpp
@@ -204,7 +204,7 @@ char checkAmmo2(PLAYER *pPlayer, int ammotype, int amount)
 
 void SpawnBulletEject(PLAYER *pPlayer, int a2, int a3)
 {
-    if ((r_mirrormode & 1) && (numplayers == 1) && !VanillaMode(true)) // mirror mode enabled, invert position for bullet ejection
+    if ((r_mirrormode & 1) && (numplayers == 1 || r_mirrormodelock) && !VanillaMode(true)) // mirror mode enabled, invert position for bullet ejection
         a2 = -a2, a3 = -a3;
     POSTURE *pPosture = &pPlayer->pPosture[pPlayer->lifeMode][pPlayer->posture];
     pPlayer->zView = pPlayer->pSprite->z-pPosture->eyeAboveZ;
@@ -220,7 +220,7 @@ void SpawnBulletEject(PLAYER *pPlayer, int a2, int a3)
 
 void SpawnShellEject(PLAYER *pPlayer, int a2, int a3)
 {
-    if ((r_mirrormode & 1) && (numplayers == 1) && !VanillaMode(true)) // mirror mode enabled, invert position for shell ejection
+    if ((r_mirrormode & 1) && (numplayers == 1 || r_mirrormodelock) && !VanillaMode(true)) // mirror mode enabled, invert position for shell ejection
         a2 = -a2, a3 = -a3;
     POSTURE *pPosture = &pPlayer->pPosture[pPlayer->lifeMode][pPlayer->posture];
     pPlayer->zView = pPlayer->pSprite->z-pPosture->eyeAboveZ;
@@ -1439,7 +1439,7 @@ void AltFireSpread2(int nTrigger, PLAYER *pPlayer)
     dassert(nTrigger > 0 && nTrigger <= kMaxSpread);
     Aim *aim = &pPlayer->aim;
     int angle;
-    if ((r_mirrormode & 1) && (numplayers == 1) && !VanillaMode(true)) // mirror mode enabled, invert tommy gun spread (only for single-player)
+    if ((r_mirrormode & 1) && (numplayers == 1 || r_mirrormodelock) && !VanillaMode(true)) // mirror mode enabled, invert tommy gun spread (only for single-player)
         angle = (getangle(aim->dx, aim->dy)-((112*(nTrigger-1))/14-56))&2047;
     else
         angle = (getangle(aim->dx, aim->dy)+((112*(nTrigger-1))/14-56))&2047;

source/build/include/baselayer.h
@@ -62,7 +62,7 @@ extern int32_t r_usenewaspect, newaspect_enable;
 extern int32_t r_fpgrouscan;
 extern int32_t setaspect_new_use_dimen;
 extern uint32_t r_screenxy;
-extern int32_t r_mirrormode;
+extern int32_t r_mirrormode, r_mirrormodelock;
 extern int32_t xres, yres, bpp, fullscreen, bytesperline;
 extern double refreshfreq;
 extern intptr_t frameplace;

source/build/src/baselayer.cpp
@@ -782,6 +782,30 @@ static int osdcmd_cvar_set_baselayer(osdcmdptr_t parm)
     return r;
 }
 
+
+static int osdfunc_mirrormode(osdcmdptr_t parm)
+{
+    static const char *const modes[] = {"off", "mirror horizontal", "mirror vertically", "mirror horizontal/vertically"};
+
+    if (parm->numparms != 1)
+        return OSDCMD_SHOWHELP;
+
+    int32_t m = Bstrtol(parm->parms[0], NULL, 10);
+
+    if (m < 0 || m > 3)
+        return OSDCMD_SHOWHELP;
+
+    if (r_mirrormodelock)
+    {
+        VLOG_F(LOG_GFX, "Mirror mode: Not in a single-player game.");
+        return OSDCMD_OK;
+    }
+    r_mirrormode = m;
+    VLOG_F(LOG_GFX, "Mirror mode set to %s.", modes[r_mirrormode]);
+
+    return OSDCMD_OK;
+}
+
 int32_t baselayer_init(void)
 {
 #ifdef _WIN32
@@ -798,7 +822,6 @@ int32_t baselayer_init(void)
         { "r_usenewaspect","enable/disable new screen aspect ratio determination code",(void *) &r_usenewaspect, CVAR_BOOL, 0, 1 },
         { "r_screenaspect","if using r_usenewaspect and in fullscreen, screen aspect ratio in the form XXYY, e.g. 1609 for 16:9",
           (void *) &r_screenxy, SCREENASPECT_CVAR_TYPE, 0, 9999 },
-        { "r_mirrormode","mirror output display: 0: off 1: mirror horizontal 2: mirror vertically 3: mirror horizontal/vertically",(void *) &r_mirrormode, CVAR_INT, 0, 3 },
         { "r_fpgrouscan","use floating-point numbers for slope rendering",(void *) &r_fpgrouscan, CVAR_BOOL, 0, 1 },
         { "r_hightile","enable/disable hightile texture rendering",(void *) &usehightile, CVAR_BOOL, 0, 1 },
         { "r_maxspritesonscreen","maximum number of sprites to draw per frame",(void *) &maxspritesonscreen, CVAR_INT, 0, MAXSPRITESONSCREEN },
@@ -837,6 +860,8 @@ int32_t baselayer_init(void)
 
     static osdcvardata_t displayindex = { "r_displayindex","index of output display",(void*)&r_displayindex, CVAR_INT | CVAR_FUNCPTR, 0, 8 };
     OSD_RegisterCvar(&displayindex, osdcmd_displayindex);
+
+    OSD_RegisterFunction("r_mirrormode", "mirror output display: 0: off 1: mirror horizontal 2: mirror vertically 3: mirror horizontal/vertically", osdfunc_mirrormode);
 
 #ifdef USE_OPENGL
     OSD_RegisterFunction("setrendermode","setrendermode <number>: sets the engine's rendering mode.\n"

source/build/src/engine.cpp
@@ -130,7 +130,7 @@ int32_t oxyaspect = -1;
 // r_usenewaspect is the cvar, newaspect_enable to trigger the new behaviour in the code
 int32_t r_usenewaspect = 1, newaspect_enable=0;
 uint32_t r_screenxy = 0;
-int32_t r_mirrormode = 0;
+int32_t r_mirrormode = 0, r_mirrormodelock = 0;
 
 int32_t r_rotatespriteinterp = 2;
 int32_t r_rotatespriteinterpquantize = 0;

source/blood/src/common_game.h
@@ -149,6 +149,8 @@ kNetGameFlagLimitMask           =  1 << 8|1 << 9|1 << 10|1 << 11|1 << 12|1 << 13
 
 kNetGameFlagSpawnSmart          =  1 << 16,
 kNetGameFlagSpawnDist           =  1 << 17,
+kNetGameFlagMirrorHoriz         =  1 << 19,
+kNetGameFlagMirrorVert          =  1 << 20,
 
 };
 

commit 1b79dd407f7f0a1f6f1206393ef66f5a587a615d

Fix mirror mode bugs introduced in fdca60783628de6a83de2eafaaf7e53476151cbc
This reworks r_mirrormodemulti to be a multiplayer only override setting

source/blood/src/blood.cpp
@@ -613,7 +613,7 @@ void StartLevel(GAMEOPTIONS *gameOptions)
         gDemo.Close();
     netWaitForEveryone(0);
     VanillaModeUpdate();
-    r_mirrormodelock = 0;
+    r_mirrormodemulti = 0;
     if (gGameOptions.nGameType == kGameTypeSinglePlayer)
     {
         if (!(gGameOptions.uGameFlags&kGameFlagContinuing))
@@ -707,12 +707,11 @@ void StartLevel(GAMEOPTIONS *gameOptions)
         gRedFlagDropped = false;
         gView = gMe;
         gViewIndex = myconnectindex;
-        r_mirrormodelock = 1;
-        r_mirrormode = 0;
+        r_mirrormodemulti = 4; // set active flag
         if (gGameOptions.uNetGameFlags&kNetGameFlagMirrorHoriz)
-            r_mirrormode |= 1;
+            r_mirrormodemulti |= 1;
         if (gGameOptions.uNetGameFlags&kNetGameFlagMirrorVert)
-            r_mirrormode |= 2;
+            r_mirrormodemulti |= 2;
     }
     if (gameOptions->uGameFlags&kGameFlagContinuing) // if episode is in progress, remember player stats
     {

source/blood/src/controls.cpp
@@ -483,7 +483,7 @@ void ctrlGetInput(void)
         if (BUTTON(gamefunc_Turn_Right))
             turnRight = 1;
     }
-    if (r_mirrormode & 1) // mirror mode (horiz), invert controls
+    if (MIRRORMODE & 1) // mirror mode (horiz), invert controls
     {
         input.strafe = -input.strafe;
         info.mousex = -info.mousex;
@@ -494,7 +494,7 @@ void ctrlGetInput(void)
         info.dx = -info.dx;
         info.dyaw = -info.dyaw;
     }
-    if ((r_mirrormode & 2) && gMouseAim) // mirror mode (vert), invert y axis for mouse aiming
+    if ((MIRRORMODE & 2) && gMouseAim) // mirror mode (vert), invert y axis for mouse aiming
     {
         info.mousey = -info.mousey;
         info.dpitch = -info.dpitch;

source/blood/src/map2d.cpp
@@ -195,7 +195,7 @@ void CViewMap::Draw(void)
     renderDrawMapView(x,y,nZoom>>2,angle);
     DrawMap(x,y,nZoom>>2,angle);
 
-    if (videoGetRenderMode() == REND_CLASSIC && (r_mirrormode & 1)) // mirror framebuffer for classic renderer
+    if (videoGetRenderMode() == REND_CLASSIC && (MIRRORMODE & 1)) // mirror framebuffer for classic renderer
         videoMirrorDrawing();
 
     char *pTitle = levelGetTitle();

source/blood/src/menu.cpp
@@ -2528,8 +2528,7 @@ void SetMirrorMode(CGameMenuItemZCycle *pItem)
 {
     if (pItem == NULL)
         pItem = &itemOptionsDisplayViewMirrorMode;
-    if (!r_mirrormodelock)
-        r_mirrormode = pItem->m_nFocus % ARRAY_SSIZE(pzMirrorModeStrings);
+    r_mirrormode = pItem->m_nFocus % ARRAY_SSIZE(pzMirrorModeStrings);
 }
 
 void SetSlowRoomFlicker(CGameMenuItemZBool *pItem)

source/blood/src/network.cpp
@@ -175,7 +175,7 @@ void netResetToSinglePlayer(void)
     gGameMenuMgr.Deactivate();
     gNetNotifyProfileUpdate = false;
     gPlayerRoundEnding = 0;
-    r_mirrormodelock = 0;
+    r_mirrormodemulti = 0;
     SetMirrorMode(NULL); // restore single-player setting for mirror mode
 }
 

source/blood/src/view.cpp
@@ -4015,7 +4015,7 @@ inline void viewAimReticle(PLAYER *pPlayer, int defaultHoriz, fix16_t q16slopeho
     int q16SlopeTilt = fix16_from_float(0.82f);
     int cX = 160;
     int cY = defaultHoriz;
-    if (!gCenterHoriz && (r_mirrormode & 1)) // offset crosshair if mirror mode is set to vertical mode
+    if (!gCenterHoriz && (MIRRORMODE & 1)) // offset crosshair if mirror mode is set to vertical mode
         cY += 19;
     cX <<= 16;
     cY <<= 16;
@@ -4026,12 +4026,12 @@ inline void viewAimReticle(PLAYER *pPlayer, int defaultHoriz, fix16_t q16slopeho
         const int q16hfov = divscale16(90, gFov);
         const int q16vfov = Blrintf(float(fix16_one) * fFov);
         int cZ = pPlayer->relAim.dy * 160 / pPlayer->relAim.dx; // calculate aiming target offset from center
-        if (r_mirrormode & 1) // mirror mode flip
+        if (MIRRORMODE & 1) // mirror mode flip
             cZ = -cZ;
         cX += mulscale16(cZ<<16, q16hfov); // scale to current fov
         cZ = mulscale16((8<<4)<<16, q16vfov)>>16; // calculate vertical fov scale
         cZ = (pPlayer->relAim.dz / cZ)<<16; // convert target z to on screen units
-        if (r_mirrormode & 2) // mirror mode flip
+        if (MIRRORMODE & 2) // mirror mode flip
             cZ = -cZ;
         cY += cZ;
 
@@ -4055,7 +4055,7 @@ inline void viewAimReticle(PLAYER *pPlayer, int defaultHoriz, fix16_t q16slopeho
     if (gSlopeTilting && (gSlopeReticle || bShowAutoAimTarget)) // adjust crosshair for slope tilting/auto aim
     {
         q16SlopeTilt = mulscale16(q16slopehoriz, q16SlopeTilt);
-        if (r_mirrormode & 2) // mirror mode flip
+        if (MIRRORMODE & 2) // mirror mode flip
             q16SlopeTilt = -q16SlopeTilt;
         cY += q16SlopeTilt;
     }
@@ -4790,7 +4790,7 @@ RORHACK:
         sub_557C4(cX, cY, gInterpolate);
         renderDrawMasks();
         gView->pSprite->cstat = bakCstat;
-        char bMirrorScreen = (videoGetRenderMode() == REND_CLASSIC) && r_mirrormode; // mirror framebuffer for classic renderer
+        char bMirrorScreen = (videoGetRenderMode() == REND_CLASSIC) && MIRRORMODE; // mirror framebuffer for classic renderer
 
         if ((videoGetRenderMode() == REND_CLASSIC) && (gRenderScale > 1) && !bRenderScaleRefresh)
         {

source/blood/src/weapon.cpp
@@ -204,7 +204,7 @@ char checkAmmo2(PLAYER *pPlayer, int ammotype, int amount)
 
 void SpawnBulletEject(PLAYER *pPlayer, int a2, int a3)
 {
-    if ((r_mirrormode & 1) && (numplayers == 1 || r_mirrormodelock) && !VanillaMode(true)) // mirror mode enabled, invert position for bullet ejection
+    if ((MIRRORMODE & 1) && !VanillaMode(true)) // mirror mode enabled, invert position for bullet ejection
         a2 = -a2, a3 = -a3;
     POSTURE *pPosture = &pPlayer->pPosture[pPlayer->lifeMode][pPlayer->posture];
     pPlayer->zView = pPlayer->pSprite->z-pPosture->eyeAboveZ;
@@ -220,7 +220,7 @@ void SpawnBulletEject(PLAYER *pPlayer, int a2, int a3)
 
 void SpawnShellEject(PLAYER *pPlayer, int a2, int a3)
 {
-    if ((r_mirrormode & 1) && (numplayers == 1 || r_mirrormodelock) && !VanillaMode(true)) // mirror mode enabled, invert position for shell ejection
+    if ((MIRRORMODE & 1) && !VanillaMode(true)) // mirror mode enabled, invert position for shell ejection
         a2 = -a2, a3 = -a3;
     POSTURE *pPosture = &pPlayer->pPosture[pPlayer->lifeMode][pPlayer->posture];
     pPlayer->zView = pPlayer->pSprite->z-pPosture->eyeAboveZ;
@@ -1439,7 +1439,7 @@ void AltFireSpread2(int nTrigger, PLAYER *pPlayer)
     dassert(nTrigger > 0 && nTrigger <= kMaxSpread);
     Aim *aim = &pPlayer->aim;
     int angle;
-    if ((r_mirrormode & 1) && (numplayers == 1 || r_mirrormodelock) && !VanillaMode(true)) // mirror mode enabled, invert tommy gun spread (only for single-player)
+    if ((MIRRORMODE & 1) && !VanillaMode(true)) // mirror mode enabled, invert tommy gun spread (only for single-player)
         angle = (getangle(aim->dx, aim->dy)-((112*(nTrigger-1))/14-56))&2047;
     else
         angle = (getangle(aim->dx, aim->dy)+((112*(nTrigger-1))/14-56))&2047;

source/build/include/baselayer.h
@@ -62,12 +62,15 @@ extern int32_t r_usenewaspect, newaspect_enable;
 extern int32_t r_fpgrouscan;
 extern int32_t setaspect_new_use_dimen;
 extern uint32_t r_screenxy;
-extern int32_t r_mirrormode, r_mirrormodelock;
+extern int32_t r_mirrormode, r_mirrormodemulti;
 extern int32_t xres, yres, bpp, fullscreen, bytesperline;
 extern double refreshfreq;
 extern intptr_t frameplace;
 extern char offscreenrendering;
 extern int32_t nofog;
+
+// NOTE: this returns either r_mirrormode or r_mirrormodemulti. r_mirrormodemulti is used for multiplayer to ensure sync across clients (4 == active flag)
+#define MIRRORMODE (r_mirrormodemulti & 4 ? r_mirrormodemulti&3 : r_mirrormode)
 
 extern int32_t r_maxfps;
 extern int32_t g_numdisplays;

source/build/src/2d.cpp
@@ -148,7 +148,7 @@ static void drawlinegl(int32_t x1, int32_t y1, int32_t x2, int32_t y2, palette_t
     glViewport(0, 0, xdim, ydim);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    if (!(r_mirrormode & 1))
+    if (!(MIRRORMODE & 1))
         glOrtho(0, xdim, ydim, 0, -1, 1);
     else // mirror mode, invert 2d view
         glOrtho(xdim, 0, ydim, 0, -1, 1);
@@ -1630,7 +1630,7 @@ void polymostSet2dView(void)
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    if (!(r_mirrormode & 1))
+    if (!(MIRRORMODE & 1))
         glOrtho(0, xdim, ydim, 0, -1, 1);
     else // mirror mode, invert 2d view
         glOrtho(xdim, 0, ydim, 0, -1, 1);

source/build/src/baselayer.cpp
@@ -787,29 +787,6 @@ static int osdcmd_cvar_set_baselayer(osdcmdptr_t parm)
     return r;
 }
 
-static int osdfunc_mirrormode(osdcmdptr_t parm)
-{
-    static const char *const modes[] = {"off", "mirror horizontal", "mirror vertically", "mirror horizontal/vertically"};
-
-    if (parm->numparms != 1)
-        return OSDCMD_SHOWHELP;
-
-    int32_t m = Bstrtol(parm->parms[0], NULL, 10);
-
-    if (m < 0 || m > 3)
-        return OSDCMD_SHOWHELP;
-
-    if (r_mirrormodelock)
-    {
-        VLOG_F(LOG_GFX, "Mirror mode: Not in a single-player game.");
-        return OSDCMD_OK;
-    }
-    r_mirrormode = m;
-    VLOG_F(LOG_GFX, "Mirror mode set to %s.", modes[r_mirrormode]);
-
-    return OSDCMD_OK;
-}
-
 int32_t baselayer_init(void)
 {
 #ifdef _WIN32
@@ -826,6 +803,7 @@ int32_t baselayer_init(void)
         { "r_usenewaspect","enable/disable new screen aspect ratio determination code",(void *) &r_usenewaspect, CVAR_BOOL, 0, 1 },
         { "r_screenaspect","if using r_usenewaspect and in fullscreen, screen aspect ratio in the form XXYY, e.g. 1609 for 16:9",
           (void *) &r_screenxy, SCREENASPECT_CVAR_TYPE, 0, 9999 },
+        { "r_mirrormode","mirror output display: 0: off 1: mirror horizontal 2: mirror vertically 3: mirror horizontal/vertically",(void *) &r_mirrormode, CVAR_INT, 0, 3 },
         { "r_fpgrouscan","use floating-point numbers for slope rendering",(void *) &r_fpgrouscan, CVAR_BOOL, 0, 1 },
         { "r_hightile","enable/disable hightile texture rendering",(void *) &usehightile, CVAR_BOOL, 0, 1 },
         { "r_maxspritesonscreen","maximum number of sprites to draw per frame",(void *) &maxspritesonscreen, CVAR_INT, 0, MAXSPRITESONSCREEN },
@@ -864,8 +842,6 @@ int32_t baselayer_init(void)
 
     static osdcvardata_t displayindex = { "r_displayindex","index of output display",(void*)&r_displayindex, CVAR_INT | CVAR_FUNCPTR, 0, 8 };
     OSD_RegisterCvar(&displayindex, osdcmd_displayindex);
-
-    OSD_RegisterFunction("r_mirrormode", "mirror output display: 0: off 1: mirror horizontal 2: mirror vertically 3: mirror horizontal/vertically", osdfunc_mirrormode);
 
 #ifdef USE_OPENGL
     OSD_RegisterFunction("setrendermode","setrendermode <number>: sets the engine's rendering mode.\n"

source/build/src/engine.cpp
@@ -130,7 +130,7 @@ int32_t oxyaspect = -1;
 // r_usenewaspect is the cvar, newaspect_enable to trigger the new behaviour in the code
 int32_t r_usenewaspect = 1, newaspect_enable=0;
 uint32_t r_screenxy = 0;
-int32_t r_mirrormode = 0, r_mirrormodelock = 0;
+int32_t r_mirrormode = 0, r_mirrormodemulti = 0;
 
 int32_t r_rotatespriteinterp = 2;
 int32_t r_rotatespriteinterpquantize = 0;

source/build/src/polymost.cpp
@@ -1493,13 +1493,13 @@ static void resizeglcheck(void)
     m[2][2] = (farclip + nearclip) / (farclip - nearclip);
     m[2][3] = 1.f;
     m[3][2] = -(2.f * farclip * nearclip) / (farclip - nearclip);
-    if (r_mirrormode & 1) // mirror mode (horiz), invert eye matrix
+    if (MIRRORMODE & 1) // mirror mode (horiz), invert eye matrix
     {
         m[0][0] = -m[0][0];
         m[1][0] = -m[1][0];
         m[2][0] = -m[2][0];
     }
-    if (r_mirrormode & 2) // mirror mode (vert), invert eye matrix
+    if (MIRRORMODE & 2) // mirror mode (vert), invert eye matrix
     {
         m[0][1] = -m[0][1];
         m[1][1] = -m[1][1];
@@ -9873,11 +9873,12 @@ int32_t polymost_printtext256(int32_t xpos, int32_t ypos, int16_t col, int16_t b
 
     polymost_usePaletteIndexing(false);
 
-    const int32_t r_mirrormode_temp = r_mirrormode; // disable mirror mode for polymostSet2dView
-    r_mirrormode = 0;
+    const int32_t r_mirrormode_temp = r_mirrormode, r_mirrormodemulti_temp = r_mirrormodemulti; // disable mirror mode for polymostSet2dView
+    r_mirrormode = r_mirrormodemulti = 0;
     polymostSet2dView();	// disables blending, texturing, and depth testing
     r_mirrormode = r_mirrormode_temp;
-    if (r_mirrormode & 1) // set flag so polymost_fillpolygon() will recall polymostSet2dView and setup mirror view
+    r_mirrormodemulti = r_mirrormodemulti_temp;
+    if (MIRRORMODE & 1) // set flag so polymost_fillpolygon() will recall polymostSet2dView and setup mirror view
         polymost2d = 0;
 
     buildgl_bindSamplerObject(0, 0);

source/build/src/sdlayer.cpp
@@ -2022,7 +2022,7 @@ void videoMirrorTile(uint8_t *pTile, int nWidth, int nHeight)
     uint8_t *pBuff = mirroredLine;
     const char bAllocBuff = nSize > sizeof(mirroredLine); // if bigger than static mirrored line, allocate from memory (very slow!)
 
-    if (!r_mirrormode || !pTile || !nSize)
+    if (!MIRRORMODE || !pTile || !nSize)
         return;
     if (bAllocBuff)
     {
@@ -2031,7 +2031,7 @@ void videoMirrorTile(uint8_t *pTile, int nWidth, int nHeight)
             return;
     }
 
-    if (r_mirrormode & 1) // mirror mode (horiz)
+    if (MIRRORMODE & 1) // mirror mode (horiz)
     {
         uint8_t *pRow, *pEnd = &pTile[(nHeight-1)*nWidth];
         for (pRow = pTile; pRow <= pEnd; pRow += nWidth)
@@ -2040,7 +2040,7 @@ void videoMirrorTile(uint8_t *pTile, int nWidth, int nHeight)
             copybufreverse((void *)&pBuff[nWidth-1], (void *)pRow, nWidth);
         }
     }
-    if (r_mirrormode & 2) // mirror mode (vert)
+    if (MIRRORMODE & 2) // mirror mode (vert)
     {
         uint8_t *pLow, *pHigh = &pTile[(nHeight-1)*nWidth];
         for (pLow = pTile; pLow < pHigh; pLow += nWidth, pHigh -= nWidth)
@@ -2062,8 +2062,6 @@ void videoMirrorDrawing(void)
     uint8_t *curFrame;
     vec2_t screenxy;
 
-    if (!r_mirrormode)
-        return;
 #ifdef USE_OPENGL
     if (!nogl)
     {

source/build/src/voxmodel.cpp
@@ -1222,7 +1222,7 @@ int32_t polymost_voxdraw(voxmodel_t *m, tspriteptr_t const tspr)
         glFrontFace(GL_CCW);
 
     buildgl_setEnabled(GL_CULL_FACE);
-    switch (r_mirrormode)
+    switch (MIRRORMODE)
     {
     case 2: // vert
     case 1: // hoz

source/build/src/winlayer.cpp
@@ -1754,7 +1754,7 @@ void videoMirrorTile(uint8_t *pTile, int nWidth, int nHeight)
     uint8_t *pBuff = mirroredLine;
     const char bAllocBuff = nSize > sizeof(mirroredLine); // if bigger than static mirrored line, allocate from memory (very slow!)
 
-    if (!r_mirrormode || !pTile || !nSize)
+    if (!MIRRORMODE || !pTile || !nSize)
         return;
     if (bAllocBuff)
     {
@@ -1763,7 +1763,7 @@ void videoMirrorTile(uint8_t *pTile, int nWidth, int nHeight)
             return;
     }
 
-    if (r_mirrormode & 1) // mirror mode (horiz)
+    if (MIRRORMODE & 1) // mirror mode (horiz)
     {
         uint8_t *pRow, *pEnd = &pTile[(nHeight-1)*nWidth];
         for (pRow = pTile; pRow <= pEnd; pRow += nWidth)
@@ -1772,7 +1772,7 @@ void videoMirrorTile(uint8_t *pTile, int nWidth, int nHeight)
             copybufreverse((void *)&pBuff[nWidth-1], (void *)pRow, nWidth);
         }
     }
-    if (r_mirrormode & 2) // mirror mode (vert)
+    if (MIRRORMODE & 2) // mirror mode (vert)
     {
         uint8_t *pLow, *pHigh = &pTile[(nHeight-1)*nWidth];
         for (pLow = pTile; pLow < pHigh; pLow += nWidth, pHigh -= nWidth)
@@ -1792,8 +1792,6 @@ void videoMirrorTile(uint8_t *pTile, int nWidth, int nHeight)
 //
 void videoMirrorDrawing(void)
 {
-    if (!r_mirrormode)
-        return;
     if (!frameplace)
         return;
     if ((xres <= 0) || (yres <= 0))