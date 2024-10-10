54dbe28f5ee16a4317f8d5d4859c37a4b5f8f18f
tmyqlfpir <80724828+tmyqlfpir@users.noreply.github.com>
2024-10-10 10:44:11 +1000

    Add support for solo axis deadzones

--- source/blood/src/config.h
+++ source/blood/src/config.h
@@ -46,6 +46,7 @@ extern int32_t JoystickAnalogueScale[MAXJOYAXES];
 extern int32_t JoystickAnalogueDead[MAXJOYAXES];
 extern int32_t JoystickAnalogueSaturate[MAXJOYAXES];
 extern int32_t JoystickAnalogueInvert[MAXJOYAXES];
+extern int32_t JoystickAnalogueIsolatedDeadZone[MAXJOYAXES];
 extern uint8_t KeyboardKeys[NUMGAMEFUNCTIONS][2];
 extern int32_t scripthandle;
 extern int32_t setupread;
