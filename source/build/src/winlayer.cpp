aad247a67539b63fd23a74c8e2b714c46981d5d0
tmyqlfpir <80724828+tmyqlfpir@users.noreply.github.com>
2024-11-01 18:49:00 +1000

    Use cache instead of alloc for mirror tile for software mode

--- source/build/src/winlayer.cpp
+++ source/build/src/winlayer.cpp
@@ -1746,21 +1746,27 @@ void videoBeginDrawing(void)
 //
 // mirrorTile() -- mirror input tile buffer
 //
-static uint8_t mirroredLine[1920*4] = {0};
-
 void videoMirrorTile(uint8_t *pTile, int nWidth, int nHeight)
 {
-    const size_t nSize = nWidth;
-    uint8_t *pBuff = mirroredLine;
-    const char bAllocBuff = nSize > sizeof(mirroredLine); // if bigger than static mirrored line, allocate from memory (very slow!)
-
-    if (!MIRRORMODE || !pTile || !nSize)
+    if (!MIRRORMODE || !pTile || nWidth <= 0)
         return;
+
+    static uint8_t buffMirroredLine[1920*4] = {0};
+    const uint32_t kMirrorTile = MAXTILES-1;
+    const char bAllocBuff = (size_t)nWidth > sizeof(buffMirroredLine); // if bigger than static mirrored line, allocate from cache
+    if (bAllocBuff && walock[kMirrorTile] != 0)
+        return;
+
+    uint8_t *pBuff;
     if (bAllocBuff)
     {
-        pBuff = (uint8_t *)Xmalloc(nSize);
-        if (!pBuff)
-            return;
+        walock[kMirrorTile] = CACHE1D_UNLOCKED;
+        g_cache.allocateBlock(&waloff[kMirrorTile], nWidth, &walock[kMirrorTile]);
+        pBuff = (uint8_t *)waloff[kMirrorTile];
+    }
+    else
+    {
+        pBuff = buffMirroredLine;
     }
 
     if (MIRRORMODE & 1) // mirror mode (horiz)
@@ -1783,7 +1789,10 @@ void videoMirrorTile(uint8_t *pTile, int nWidth, int nHeight)
         }
     }
     if (bAllocBuff)
-        Bfree(pBuff);
+    {
+        walock[kMirrorTile] = 0;
+        waloff[kMirrorTile] = 0;
+    }
 }
 
 
