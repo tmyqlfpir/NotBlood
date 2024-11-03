5ed95b34814b84822ad30cebe87cf3a8db9e2793
tmyqlfpir <80724828+tmyqlfpir@users.noreply.github.com>
2024-11-03 13:57:22 +1000

    Don't bother dereferencing xsprite for non-modern maps

--- source/blood/src/actor.cpp
+++ source/blood/src/actor.cpp
@@ -6735,7 +6735,6 @@ spritetype * actSpawnSprite(spritetype *pSource, int nStat);
 
 spritetype *actSpawnDude(spritetype *pSource, short nType, int a3, int a4)
 {
-    XSPRITE* pXSource = &xsprite[pSource->extra];
     spritetype *pSprite2 = actSpawnSprite(pSource, kStatDude);
     if (!pSprite2) return NULL;
     XSPRITE *pXSprite2 = &xsprite[pSprite2->extra];
@@ -6771,6 +6770,7 @@ spritetype *actSpawnDude(spritetype *pSource, short nType, int a3, int a4)
     // add a way to inherit some values of spawner type 18 by dude.
     // This way designer can count enemies via switches and do many other interesting things.
     if (gModernMap && pSource->flags & kModernTypeFlag1) {
+        XSPRITE* pXSource = &xsprite[pSource->extra];
         switch (pSource->type) { // allow inheriting only for selected source types
             case kMarkerDudeSpawn:
                 //inherit pal?
