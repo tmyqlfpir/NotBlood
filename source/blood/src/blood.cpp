ef0ad3068a3550d280739273cc29d516c82a0f4b
tmyqlfpir <80724828+tmyqlfpir@users.noreply.github.com>
2024-12-21 16:47:24 +1000

    Drop hand enemy after killing player for multiplayer

--- source/blood/src/blood.cpp
+++ source/blood/src/blood.cpp
@@ -878,8 +878,8 @@ void StartLevel(GAMEOPTIONS *gameOptions)
         }
         else if ((gGameOptions.nGameType == kGameTypeTeams) && !VanillaMode()) // if ctf mode and went to next level, reset scores
             playerResetScores(i);
-        gChokeCounter[i] = 0;
         playerStart(i, 1);
+        gChokeCounter[i] = 0;
     }
     playerInitRoundCheck();
     if (gameOptions->uGameFlags&kGameFlagContinuing) // if episode is in progress, restore player stats
